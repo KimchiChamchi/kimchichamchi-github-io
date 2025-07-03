0b57210569d34f0794f4a93349a1892ef6638432
KimchiChamchi <esm2103@gmail.com>
2025-07-03 15:02:50 +0900

    수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+const repoName = "kimchichamchi-github-io"; // 깃허브 레포지토리명
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
